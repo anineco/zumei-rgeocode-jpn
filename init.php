69b105e7c74404be0cbe52f11dc348b5d3b9acef
Anineco Nyanta <anineco@nifty.com>
2023-12-24 07:03:17 +0900

    replaced by .my.cnf

--- init.php
+++
@@ -1,9 +0,0 @@
-<?php
-function set_init() {
-  return array(
-    'user'     => '_YOUR_DB_USERNAME_',
-    'password' => '_YOUR_DB_PASSWORD_',
-    'host'     => '_YOUR_DB_HOST_',
-    'database' => '_YOUR_DATABASE_'
-  );
-}
